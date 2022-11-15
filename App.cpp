e332e6e65e7bf6db795727827c0ce1471a24490f
Eddie Gong <EddieGong@users.noreply.github.com>
2022-11-15 23:34:37 +0100

    Remove unused sample code
    + The code was generated by the vs template. Not useful for a d3d app, so remove it

--- App.cpp
+++ App.cpp
@@ -2,21 +2,19 @@
 
 #include <CLRMain.h>
 
-using namespace winrt;
+using namespace winrt::Windows;
+using namespace winrt::Windows::ApplicationModel::Core;
+using namespace winrt::Windows::Foundation::Numerics;
+using namespace winrt::Windows::UI;
+using namespace winrt::Windows::UI::Core;
+using namespace winrt::Windows::UI::Input;
 
-using namespace Windows;
-using namespace Windows::ApplicationModel::Core;
-using namespace Windows::Foundation::Numerics;
-using namespace Windows::UI;
-using namespace Windows::UI::Core;
-using namespace Windows::UI::Composition;
-
-using namespace Windows::ApplicationModel;
-using namespace Windows::ApplicationModel::Activation;
+using namespace winrt::Windows::ApplicationModel;
+using namespace winrt::Windows::ApplicationModel::Activation;
 
 using namespace CLR;
 
-struct App : implements<App, IFrameworkViewSource, IFrameworkView>
+struct App : winrt::implements<App, IFrameworkViewSource, IFrameworkView>
 {
     IFrameworkView CreateView()
     {
@@ -30,14 +28,16 @@ struct App : implements<App, IFrameworkViewSource, IFrameworkView>
         CoreApplication::Suspending({ this, &App::OnSuspending });
 
         CoreApplication::Resuming({ this, &App::OnResuming });
-    }
 
-    void Load(hstring const&)
-    {
         if (main == nullptr)
         {
             main = winrt::make_self<CLRMain>();
         }
+    }
+
+    void Load(winrt::hstring const& /* entryPoint */)
+    {
+        main->Load();
     }
 
     void Uninitialize()
@@ -46,113 +46,16 @@ struct App : implements<App, IFrameworkViewSource, IFrameworkView>
 
     void Run()
     {
-        CoreWindow window = CoreWindow::GetForCurrentThread();
-        window.Activate();
-
-        CoreDispatcher dispatcher = window.Dispatcher();
-        dispatcher.ProcessEvents(CoreProcessEventsOption::ProcessUntilQuit);
+        main->Run();
     }
 
     void SetWindow(CoreWindow const& window)
     {
-        Compositor compositor;
-        ContainerVisual root = compositor.CreateContainerVisual();
-        m_target = compositor.CreateTargetForCurrentView();
-        m_target.Root(root);
-        m_visuals = root.Children();
+        window.PointerCursor(CoreCursor(CoreCursorType::Arrow, 0));
 
-        window.PointerPressed({ this, &App::OnPointerPressed });
-        window.PointerMoved({ this, &App::OnPointerMoved });
-
-        window.PointerReleased([&](auto && ...)
-        {
-            m_selected = nullptr;
-        });
-    }
-
-    void OnPointerPressed(IInspectable const&, PointerEventArgs const& args)
-    {
-        float2 const point = args.CurrentPoint().Position();
-
-        for (Visual visual : m_visuals)
-        {
-            float3 const offset = visual.Offset();
-            float2 const size = visual.Size();
-
-            if (point.x >= offset.x &&
-                point.x < offset.x + size.x &&
-                point.y >= offset.y &&
-                point.y < offset.y + size.y)
-            {
-                m_selected = visual;
-                m_offset.x = offset.x - point.x;
-                m_offset.y = offset.y - point.y;
-            }
-        }
-
-        if (m_selected)
-        {
-            m_visuals.Remove(m_selected);
-            m_visuals.InsertAtTop(m_selected);
-        }
-        else
-        {
-            AddVisual(point);
-        }
-    }
-
-    void OnPointerMoved(IInspectable const&, PointerEventArgs const& args)
-    {
-        if (m_selected)
-        {
-            float2 const point = args.CurrentPoint().Position();
-
-            m_selected.Offset(
-                {
-                    point.x + m_offset.x,
-                    point.y + m_offset.y,
-                    0.0f
-                });
-        }
-    }
-
-    void AddVisual(float2 const point)
-    {
-        Compositor compositor = m_visuals.Compositor();
-        SpriteVisual visual = compositor.CreateSpriteVisual();
-
-        static Color colors[] =
-        {
-            { 0xDC, 0x5B, 0x9B, 0xD5 },
-            { 0xDC, 0xED, 0x7D, 0x31 },
-            { 0xDC, 0x70, 0xAD, 0x47 },
-            { 0xDC, 0xFF, 0xC0, 0x00 }
-        };
-
-        static unsigned last = 0;
-        unsigned const next = ++last % _countof(colors);
-        visual.Brush(compositor.CreateColorBrush(colors[next]));
-
-        float const BlockSize = 100.0f;
-
-        visual.Size(
-            {
-                BlockSize,
-                BlockSize
-            });
-
-        visual.Offset(
-            {
-                point.x - BlockSize / 2.0f,
-                point.y - BlockSize / 2.0f,
-                0.0f,
-            });
-
-        m_visuals.InsertAtTop(visual);
-
-        m_selected = visual;
-        m_offset.x = -BlockSize / 2.0f;
-        m_offset.y = -BlockSize / 2.0f;
+        PointerVisualizationSettings visualizationSettings{ PointerVisualizationSettings::GetForCurrentView() };
+        visualizationSettings.IsContactFeedbackEnabled(false);
+        visualizationSettings.IsBarrelButtonFeedbackEnabled(false);
     }
 
     void OnActivated(CoreApplicationView const& /* applicationView */, IActivatedEventArgs const& /* args */)
@@ -188,16 +91,11 @@ struct App : implements<App, IFrameworkViewSource, IFrameworkView>
         //m_main->Resume();
     }
 
-    CompositionTarget m_target{ nullptr };
-    VisualCollection m_visuals{ nullptr };
-    Visual m_selected{ nullptr };
-    float2 m_offset{};
-
 private:
     winrt::com_ptr<CLRMain> main;
 };
 
 int __stdcall wWinMain(HINSTANCE, HINSTANCE, PWSTR, int)
 {
-    CoreApplication::Run(make<App>());
+    CoreApplication::Run(winrt::make<App>());
 }
